e641f1d10408fd85a2bdefe1bb4d39684554148c
shrikant <shrisurve7757@gmail.com>
2024-07-18 18:52:38 +0530

    commit

--- src/collection/list/ArrayListDemo.java
+++ src/collection/list/ArrayListDemo.java
@@ -2,7 +2,8 @@ package collection.list;
 
 public class ArrayListDemo
 {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args)
+    {
+        System.out.printf("");
     }
 }
